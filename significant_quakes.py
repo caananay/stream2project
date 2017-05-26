c4a9622be9323ccbfd7252370929656185227ece
Ca-ananay <caananay.maduka@gmail.com>
2017-05-26 21:32:56 +0100

    changed d3.js from ver 4 to 3 modified graph.js and significant_quakes.py

--- significant_quakes.py
+++ significant_quakes.py
@@ -30,7 +30,7 @@ def quake_projects():
     FIELDS = {
         '_id': False, 'YEAR': True, 'FOCAL_DEPTH': True,
         'EQ_PRIMARY': True, 'EQ_MAG_MW': True,
-        'EQ_MAG_MS': True, 'EQ_MAG_MB': True, 'COUNTRY': True, 'LOCATION': True,
+        'EQ_MAG_MS': True, 'EQ_MAG_MB': True, 'COUNTRY': True, 'LOCATION_NAME': True,
         'LATITUDE': True, 'LONGITUDE': True,
         'TOTAL_DEATHS': True, 'TOTAL_INJURIES': True, 'TOTAL_HOUSES_DESTROYED': True,
         'TOTAL_HOUSES_DAMAGED': True
--- static/js/graph.js
+++ static/js/graph.js
@@ -13,15 +13,12 @@ function makeGraphs(error, projectsJson) {
        d["TOTAL_DEATHS"] = +d["TOTAL_DEATHS"];
    });
 
-console.log(sigQuakesProjects);
    //Create a Crossfilter instance
    var ndx = crossfilter(sigQuakesProjects);
-    //console.log(ndx);
+
    //Define Dimensions
 
-   var yearDim = ndx.dimension(function (d) {
-       return d["YEAR"];
-   });
+
    var focalDepthDim = ndx.dimension(function (d) {
        return d["FOCAL_DEPTH"];
    });
@@ -38,19 +35,13 @@ console.log(sigQuakesProjects);
    var eqMagMbDim = ndx.dimension(function (d) {
        return d["EQ_MAG_MB"];
    });
-   var countryDim = ndx.dimension(function (d) {
-       return d["COUNTRY"];
-   });
    var locationDim = ndx.dimension(function (d) {
-       return d["LOCATION"];
+       return d["LOCATION_NAME"];
+   });
+var idDim = ndx.dimension(function (d) {
+       return d["I_D"];
    });
 
-   var latitudeDim = ndx.dimension(function (d) {
-       return d["LATITUDE"];
-   });
-   var longitudeDim = ndx.dimension(function (d) {
-       return d["LONGITUDE"];
-   });
    var totalDeathsDim = ndx.dimension(function (d) {
        return d["TOTAL_DEATHS"];
    });
@@ -65,22 +56,120 @@ console.log(sigQuakesProjects);
    });
 
 //Calculate metrics
-   var totalDeaths = totalDeathsDim.group();
+   //var totalDeaths = totalDeathsDim.group();
+     var countryDim = ndx.dimension(function (d) {return d["COUNTRY"];});
    var deathsByCountry = countryDim.group().reduceSum(function (d) {
         return d["TOTAL_DEATHS"];
     });
+    var deathsByLocation = locationDim.group().reduceSum(function (d) {
+        return d["TOTAL_DEATHS"];});
+     var allLocations = sigQuakesProjects.map(d => d['LOCATION_NAME']);
+  var uniqueLocations = Array.from(new Set(allLocations)).sort();
+   // var locationGroup = locationDim.group();
 
-   //Charts
+   //Bar Charts
    var deathBarChart = dc.barChart("#deaths-bar-chart");
 
+   var allCountries = sigQuakesProjects.map(d => d['COUNTRY']);
+  var uniqueCountries = Array.from(new Set(allCountries)).sort();
+
+  //row chart country
+  var  countryGroup = countryDim.group();
+
+  //Pie Chart year
+
+      var yearDim = ndx.dimension(function (d) {
+       return d["YEAR"];
+   });
+      var yearGroup = yearDim.group();
+
+    //table
+
+
    deathBarChart
-       .width(800)
+      .width(800)
        .height(200)
        .margins({top: 10, right: 50, bottom: 30, left: 50})
-       .dimension(countryDim)
-       .group(deathsByCountry)
-       .x(d3.scale.ordinal().domain(['PAKISTAN','INDONESIA', 'SPAIN','ALGERIA']));
+       .dimension(locationDim)
+       .group(deathsByLocation)
+       .x(d3.scale.ordinal().domain(uniqueLocations))
+       // .dimension(countryDim)
+       // .group(deathsByCountry)
+       //.x(d3.scale.ordinal().domain(uniqueCountries))
+        .xUnits(dc.units.ordinal);
 
+   var pieChart = dc.pieChart("#year-pie-chart")
+       .height(600)
+       .radius(150)
+       .innerRadius(50)
+       .dimension(yearDim)
+       .group(yearGroup);
+
+   var countryChart = dc.rowChart("#country-chart")
+        .height(300)
+       .width(600)
+       //.innerRadius(50)
+       .dimension(countryDim)
+       .group(countryGroup)
+        .x(d3.scale.linear().domain([0, 50]))
+       .elasticX(true);
+   //.legend(dc.legend());
+
+     var bubbleDim = ndx.dimension(function (d) {
+       return [d.FOCAL_DEPTH, d.EQ_PRIMARY];
+   });
+
+     var bubbleGroup = bubbleDim.group();
+     var minDepth = focalDepthDim.bottom(1)[0].FOCAL_DEPTH;
+     var maxDepth = focalDepthDim.top(1)[0].FOCAL_DEPTH;
+
+     var minMag = eqPrimaryDim.bottom(1)[0].EQ_PRIMARY;
+     var maxMag = eqPrimaryDim.top(1)[0].EQ_PRIMARY;
+
+      var minDeath = totalDeathsDim.bottom(1)[0].TOTAL_DEATHS;
+     var maxDeath = totalDeathsDim.top(1)[0].TOTAL_DEATHS;
+
+    var bubble = dc.bubbleChart("#total-chart")
+        .width(900)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+       .dimension(bubbleDim)
+       .group(bubbleGroup)
+        .colorAccessor(function (d) {return d.value;})
+        .colors(d3.scale.category20b())
+         .keyAccessor(function (d) {
+             return d.key[0];
+         })
+        .valueAccessor(function (d) {
+             return d.key[1];
+         })
+        .radiusValueAccessor(function (d) {
+            return d.value;
+        })
+        .maxBubbleRelativeSize(0.1)
+        .yAxisLabel("EQ PRIMARY")
+        .xAxisLabel("FOCAL DEPTH")
+        .title(function (d) {
+            return 'x: ' +d.key[0] + ', y: ' +d.key[1]+', val: '+d.value;
+        })
+        .r(d3.scale.linear().domain([minDeath,maxDeath]))
+        .y(d3.scale.linear().domain([minMag,maxMag]))
+
+        .x(d3.scale.linear().domain([minDepth,maxDepth]));
+
+
+   var dataTable = dc.dataTable("#sigQuake-table")
+       .width(900)
+       .height(1000)
+       .dimension(idDim)
+       .group(function (d) {return d.YEAR; })
+       .columns(['YEAR', 'COUNTRY', 'LOCATION_NAME', 'FOCAL_DEPTH', 'EQ_PRIMARY', 'EQ_MAG_MB', 'TOTAL_DEATHS', 'TOTAL_INJURIES', 'TOTAL_HOUSES_DESTROYED',
+        'TOTAL_HOUSES_DAMAGED'])
+       .on("renderlet", function(table){
+           table.selectAll('.dc-table-group').classed('info',true);
+       })
+      .sortBy(function(d){return d.COUNTRY})
+      .order(d3.ascending);
 
 
    dc.renderAll();
